6cd4b0510de8c5def92bbdfc86a9575717f3854b
DarianGrabino <dariangra@gmail.com>
2023-09-22 16:37:10 -0700

    fix semistandard, writes a string to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], (error) => {
-    if (error) {
-        console.error(error);
-    }
+  if (error) {
+    console.error(error);
+  }
 });
