c19cf58581e09bda836e9995446a06387eb995d5
DarianGrabino <dariangra@gmail.com>
2023-09-22 21:13:19 -0700

    computes the number of tasks completed by user id of a url, fix test 0 completed

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -3,6 +3,10 @@
 const request = require('request');
 const dict = {};
 
+for (let userId = 1; userId <= 10; userId++) {
+  dict[userId] = 0;
+}
+
 request(process.argv[2], (error, response, data) => {
   if (error) {
     console.error(error);
